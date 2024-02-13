56681f26b57dc87d2a1675d346ee8051129bc71a
Alejandro Pacas <sebastian.pacas@gmail.com>
2024-02-13 15:15:40 -0600

    Fin del ruteo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home';
+import About from './pages/about';
+import Profile from './pages/profile';
+import Sigin from './pages/sigin';
+import Signout from './pages/singout';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path = "/" element={<Home />} />
+        <Route path = "/sign-in" element={<Sigin />} />
+        <Route path = "/sign-out" element={<Signout />} />
+        <Route path = "/profile" element={<Profile />} />
+        <Route path = "/about" element={<About />} />
+
+      </Routes>
+    </BrowserRouter>
   )
+  
 }

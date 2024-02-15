5607b184722118d3483fbd5f252f4cac02ae9842
Alejandro Pacas <sebastian.pacas@gmail.com>
2024-02-15 11:02:29 -0600

    se creo un nuevo cliente, porq el primero se para de de uñas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,20 @@ import About from './pages/about';
 import Profile from './pages/profile';
 import Sigin from './pages/sigin';
 import Signout from './pages/singout';
+import Header from './components/Header';
+
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
 
         <Route path = "/" element={<Home />} />
         <Route path = "/sign-in" element={<Sigin />} />
         <Route path = "/sign-out" element={<Signout />} />
         <Route path = "/profile" element={<Profile />} />
+        <Route path = "/about" element={<About />} />
         <Route path = "/about" element={<About />} />
 
       </Routes>
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <h1>
+            <span className='text-gray-500'>Shand</span>
+            <span className='text-gray-600'>Estate</span>
+        </h1>
+    </header>
+  )
+}
